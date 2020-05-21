0c58dafd4a374da1ca60a53a2c8f89ba1790e98d
Marta Kaplinska <mk.marta96@gmail.com>
2020-05-21 18:34:08 +0200

    gyroscope settings

---
+++ Assets/GyroCam.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCam : MonoBehaviour
+{
+    private Gyroscope gyroscope;
+    private bool gyroSupported;
+    private Quaternion rotfix;
+
+    [SerializeField]
+    private Transform worldObj;
+    private float startY;
+    void Start()
+    {
+        gyroSupported = SystemInfo.supportsGyroscope;
+
+        GameObject camParent = new GameObject("camParent");
+        camParent.transform.position = transform.position;
+        transform.parent = camParent.transform; 
+
+        if (gyroSupported)
+        {
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+
+            camParent.transform.rotation = Quaternion.Euler(90f, 180f, 0f);
+            rotfix = new Quaternion(0, 0, 1, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gyroSupported && startY == 0)
+        {
+            ResetGyroRotation();
+        }
+        transform.localRotation = gyroscope.attitude * rotfix;
+    }
+
+    void ResetGyroRotation()
+    {
+        startY = transform.eulerAngles.y;
+        worldObj.rotation = Quaternion.Euler(0f, startY, 0f);
+    }
+}
