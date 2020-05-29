be40b430011094928977d1fe49a6b40414eddc48
Marta Kaplinska <mk.marta96@gmail.com>
2020-05-29 10:44:49 +0200

    xp bonus

---
+++ Assets/Models/Field Objects/POA XP bonus/XPBonus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPBonus : MonoBehaviour
+{
+    [SerializeField] private int bonus = 10;
+
+    private void OnMouseDown()
+    {
+        GameManager.Instance.CurrentPlayer.AddXp(bonus);
+        Destroy(gameObject);
+    }
+}
