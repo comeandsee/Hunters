407f5319554efdf506cc73f308793aa7016c29dd
Marta Kaplinska <mk.marta96@gmail.com>
2020-06-26 00:29:57 +0200

    audio in XPBonus and capture scene

---
+++ Assets/Scripts/POA XP bonus/XPBonus.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class XPBonus : MonoBehaviour
+{
+    [SerializeField] private int bonus = 20;
+    [SerializeField] private AudioClip clickSound;
+
+    private AudioSource audioSource;
+
+    public AudioSource AudioSource { get => audioSource; set => audioSource = value; }
+
+    private void Awake()
+    {
+        var a = GetComponents<AudioSource>();
+        AudioSource = GetComponent<AudioSource>();
+
+        Assert.IsNotNull(clickSound);
+        Assert.IsNotNull(AudioSource);
+    }
+        private void OnMouseDown()
+    {
+        AudioSource.PlayOneShot(clickSound);
+        Invoke("AddBonusAndDelete", 1.0f) ;
+    }
+
+    private void AddBonusAndDelete()
+    {
+        GameManager.Instance.CurrentPlayer.AddXp(bonus);
+        Destroy(gameObject);
+    }
+
+
+
+}
